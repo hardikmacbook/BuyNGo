3a4799669e9b9a028e7fd0afb6f992961b4d4bbb
blaze <hardikkamaliya0000@gmail.com>
2025-06-28 11:17:17 +0530

    created routing for error page and make a enter defult code for testing

---
+++ pages/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div className='p-28'>Error</div>
+  )
+}
+
+export default Error
--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import Contact from '../pages/Contact';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './componets/HomePage/Navbar/Header';
 import AutoTitle from './componets/AutoTitle';
+import Error from '../pages/Error';
 const App = () => {
   // Wrap each component
   const HomeWithTitle = AutoTitle(Home);
   const AboutWithTitle = AutoTitle(About);
   const ProductWithTitle = AutoTitle(Product);
   const ContactWithTitle = AutoTitle(Contact);
+  const ErrorWithTitle = AutoTitle(Error);
 
   return (
     <BrowserRouter>
@@ -21,6 +23,7 @@ const App = () => {
         <Route path="/about" element={<AboutWithTitle />} />
         <Route path="/products" element={<ProductWithTitle />} />
         <Route path="/contact" element={<ContactWithTitle />} />
+        <Route path="*" element={<ErrorWithTitle />} />
       </Routes>
     </BrowserRouter>
   );
