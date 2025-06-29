3c78b4c1ab244205021fdf3ab216fe8d5685185c
blaze <hardikkamaliya0000@gmail.com>
2025-06-29 13:15:54 +0530

    slider textoverlay fix for responsive

--- src/componets/HomePage/Slider/MediaSlider.jsx
+++ src/componets/HomePage/Slider/MediaSlider.jsx
@@ -156,8 +156,8 @@ const MediaSlider = () => {
 
         {/* Text Overlay for Images Only */}
         {!isVideo && (
-          <div className="absolute inset-0 flex items-end justify-start p-3 sm:p-6 lg:p-12">
-            <div className="max-w-3xl space-y-3 sm:space-y-6 animate-fade-in pb-35 sm:pb-80 lg:pb-30 sm:p-10 pl-15 sm:pl-15">
+          <div className="absolute inset-0 flex items-end justify-start p-6 sm:p-6 lg:p-12">
+            <div className="max-w-3xl space-y-3 sm:space-y-6 animate-fade-in pb-20 sm:pb-80 lg:pb-10 sm:p-10 pl-10 sm:pl-15">
               {/* Category Badge */}
               <div className="inline-flex items-center px-3 py-1 sm:px-4 sm:py-2 rounded-full bg-white/10 backdrop-blur-md border border-white/20 shadow-2xl">
                 <span className="text-white text-xs sm:text-sm font-medium">
