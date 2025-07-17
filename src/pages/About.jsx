dac7f2d4b7d1d9915b015c78afbf0b77dfa04e49
blaze <hardikkamaliya0000@gmail.com>
2025-07-17 10:48:06 +0530

    saparating contact

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import TimeLine from '../componets/TimeLine'
-import CompanyStory from '../componets/Story'
+import TimeLine from '../componets/About/Story'
+import CompanyStory from '../componets/About/TimeLine'
 
 const About = () => {
   return (
