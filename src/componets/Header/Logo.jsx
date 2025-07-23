e25b1215eafd93e9b3a9319d47f762a15d5ea151
blaze <hardikkamaliya0000@gmail.com>
2025-07-23 12:33:03 +0530

    change store name to buyNGo

--- src/componets/Header/Logo.jsx
+++ src/componets/Header/Logo.jsx
@@ -6,13 +6,14 @@ const Logo = () => {
   return (
     <>
       <Link to={"/"}>
-        <img
+        {/* <img
           className="w-[100px] sm:w-[80px] lg:w-[120px] h-[30px] sm:h-[40px] lg:h-[33px] object-contain transition-all duration-300"
           src={AvinyaLogo}
           alt="avinya logo"
           width="180"
           height="80"
-        />
+        /> */}
+        <span className="font-bold italic">BuyNGO</span>
       </Link>
     </>
   );
